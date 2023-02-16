12187159aa788970e79ec4ba4b3c1320780df5cd
Umo Akwang <akwangumo@gmail.com>
2023-02-16 04:07:54 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C programming using the puts function to print lines of code
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
