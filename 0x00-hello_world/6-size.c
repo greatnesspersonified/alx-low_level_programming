6fa76cd4fc97df5eef6412c33e6f759678c886ea
Umo Akwang <akwangumo@gmail.com>
2023-02-16 06:34:17 -0800

    5-printf.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A C programme that prints the size of various types on the computer it is compiled and run on
+ * * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
