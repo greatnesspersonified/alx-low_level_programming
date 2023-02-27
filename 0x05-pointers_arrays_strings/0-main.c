d1e28cad0682d6b5cd9bb1e02e85945095ccf490
Umo Akwang <akwangumo@gmail.com>
2023-02-27 12:55:09 -0800

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
+ * main - check the code
  *
  * Return: Always 0.
  */
@@ -14,5 +14,5 @@ int main(void)
 	printf("n=%d\n", n);
 	reset_to_98(&n);
 	printf("n=%d\n", n);
-return (0);
+	return (0);
 }
