44b34d978e84017cb5f5ccd64e8742c9c4d0bfe6
Umo Akwang <akwangumo@gmail.com>
2023-03-06 12:27:38 -0800

    main.h

---
+++ 0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print out _putchaar using printf
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	putcher('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
