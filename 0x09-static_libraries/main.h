8939613d59970268f421ea985d7dae38adafd623
Umo Akwang <akwangumo@gmail.com>
2023-03-11 21:07:17 -0800

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,14 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/*
- * File: main.h
- * Auth: IA
- * Desc: Header file containing the prototypes of all functions
- *       included in the static library libholberton.a.
- */
-
-int _putchar(char c);
+		
+int _putchar(char c);		
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
@@ -28,5 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
