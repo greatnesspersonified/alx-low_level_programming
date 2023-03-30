b72296519e248450de9cf710a44d8c431346a498
Umo Akwang <akwangumo@gmail.com>
2023-03-30 16:27:32 -0700

    List.h

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,9 +1,9 @@
-#ifndef LINKED
-#define LINKED
+#ifndef LIST_H
+#define LIST_H
 
-#include <stddef.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,10 +20,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /*LINKED*/
+#endif /* LIST_H */
